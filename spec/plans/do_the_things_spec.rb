4952ea4470d6c5137e960075e5dd43496393c514
Chris Tessmer <chris.tessmer@onyxpoint.com>
2019-11-22 20:30:58 +0000

    Keep CI happy

--- spec/plans/do_the_things_spec.rb
+++ spec/plans/do_the_things_spec.rb
@@ -1,18 +1,19 @@
 require 'spec_helper'
 
-describe "foo::do_the_things" do
+return unless defined?(BoltSpec::Plans) # puppet 5 pipelines can't include bolt
+
+describe 'foo::do_the_things' do
   BoltSpec::Plans.init
-  include BoltSpec::Plans  # <-- needed to make `run_plan` available in examples
-  it 'should return' do
-    puts '','='*80,"modulepath:\n#{modulepath}",'='*80,''
-    puts '','='*80,"config.modulepath:\n#{config.modulepath}",'='*80,''
-    puts '',"==== modulepath == config.modulepath: '#{modulepath == config.modulepath}'"
+  include BoltSpec::Plans # <-- needed to make `run_plan` available in examples
+  it 'returns' do
+    puts '', '=' * 80, "modulepath:\n#{modulepath}", '=' * 80, ''
+    puts '', '=' * 80, "config.modulepath:\n#{config.modulepath}", '=' * 80, ''
+    puts '', "==== modulepath == config.modulepath: '#{modulepath == config.modulepath}'"
     modulepath.each do |path|
-      file = File.join(path,'foo','do_the_things.pp')
+      file = File.join(path, 'foo', 'do_the_things.pp')
       puts "!!!! Found #{file}\n!!!! modulepath: #{modulepath}" if File.exist?(file)
     end
-    allow_task('my_task').always_return({'result_key' => 10})
-    expect(run_plan('foo::do_the_things', { 'param1' => 10 })).to be_ok
+    allow_task('my_task').always_return('result_key' => 10)
+    expect(run_plan('foo::do_the_things', 'param1' => 10)).to be_ok
   end
 end
-
--- spec/spec_helper_local.rb
+++ spec/spec_helper_local.rb
@@ -1,2 +1,7 @@
-require 'bolt_spec/plans'
-#BoltSpec::Plans.init
+begin
+  require 'bolt_spec/plans'
+  # BoltSpec::Plans.init
+rescue LoadError => e
+  warn e.message
+  warn '=== bolt tests will not run; ensure bolt gem is installed (requires Puppet 6+)'
+end
